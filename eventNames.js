730f56800bfcc95c8faa746e95e068feca47b6c5
Prabhas Joshi <joshi.prabhas@gmail.com>
2020-08-27 18:19:28 +0530

    Create eventNames.js

---
+++ eventNames.js
@@ -0,0 +1,13 @@
+const EV = {
+  FETCH_FROM_ROOT: 'fetchCodeBlocksFromRootPath',
+  SET_WORKING_FILE: 'setWorkingFile',
+  FETCH_CODE_BLOCK: 'fetchCodeBlock',
+  SET_CODE_BLOCK: 'setCodeBlock',
+  FETCH_NEXT_FILE: 'fetchNextFile',
+  FIND_PROPS: 'codeBlockToFindProps',
+  PROPS_RESULT: 'propsResult',
+  PERSIST_PROPS: 'persistProps',
+  PERSIST_SUCCESS: 'persistSuccess',
+};
+
+export default EV;
